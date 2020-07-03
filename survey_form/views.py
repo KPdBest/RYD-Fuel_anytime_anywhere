7f881b003e2c732596d4feb5fcafe3c9b0258d3a
KAUSTUBH PANDEY <kaustubh.10.pandey@gmail.com>
2020-07-03 20:40:52 +0530

    Update views.py

--- survey_form/views.py
+++ survey_form/views.py
@@ -11,7 +11,8 @@ from survey_form.models import SurveyData
 def user_data(request):
     if request.method == 'GET':
         user_id = request.session.get('userid')  ###Fetch UserID from the session and show the respective data #####
-        users_data=list(SurveyData.object.filter(id=user_id).values())
+        user_id=1  ##### Static as sessions is not created ########
+        users_data=list(SurveyData.objects.filter(id=user_id).values())
         if (len(users_data)>0):
             data={'Data':users_data,'responseType': 'success', 'messageType': 'success'}
             Status=status.HTTP_200_OK
@@ -21,13 +22,13 @@ def user_data(request):
 
     elif request.method == 'POST':
         data = json.loads(request.body)
-        name=request.data['name']
-        date=request.data['date']
-        organisation=request.data['organisation']
-        designation=request.data['designation']
-        city=request.data['city']
-        age=request.data['age']
-        remark=request.data['remark']
+        name=data['name']
+        date=data['date']
+        organisation=data['organisation']
+        designation=data['designation']
+        city=data['city']
+        age=data['age']
+        remark=data['remark']
 
         qry = SurveyData.objects.create(name=name,city=city,designation=designation,organisation=organisation,age=age,date=date,remark=remark)
         data={'responseType': 'success', 'messageType': 'Successfully Inserted'}
@@ -36,13 +37,15 @@ def user_data(request):
     elif request.method == 'PUT':
         ###### Let's Assume the User is updating these few fields( Designation , City , Age , Date ) in Survey Form #####################
         user_id = request.session.get('userid')   ###Fetch UserID from the session and update the respective data #####
-        designation=request.data['designation']
-        date=request.data['date']
-        city=request.data['city']
-        age=request.data['age']
+        user_id =1
+        data = json.loads(request.body)
+        designation=data['designation']
+        date=data['date']
+        city=data['city']
+        age=data['age']
 
         qry=SurveyData.objects.filter(id=user_id).update(city=city,designation=designation,age=age,date=date)
-        data={'responseType': 'success', 'messageType': 'Successfully Inserted'}
+        data={'responseType': 'success', 'messageType': 'Successfully Updated'}
         Status=status.HTTP_201_CREATED
 
-    return Response(data,status=Status)
+    return JsonResponse(data,status=Status)
